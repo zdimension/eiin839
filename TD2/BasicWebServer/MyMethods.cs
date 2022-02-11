50867130ae41f972d0880982ab174b862e8c93c2
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2022-02-11 09:50:58 +0100

    let's not make it static

--- TD2/BasicWebServer/MyMethods.cs
+++ TD2/BasicWebServer/MyMethods.cs
@@ -2,7 +2,7 @@
 
 public class MyMethods
 {
-    public static string Bidule(string nom, string prenom)
+    public string Bidule(string nom, string prenom)
     {
         return $"<html><body>Bonjour {prenom} {nom}</body></html>";
     }
